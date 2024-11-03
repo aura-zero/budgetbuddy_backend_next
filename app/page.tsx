6ec018dd99ad91dcc55b04be2248718559d1b42c
Abhishek Yadav <akyv2.5@gmail.com>
2024-11-03 16:43:54 +0530

    updated user groups and transetion

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import CreateGroup from "@/components/CreateGroup";
 import UserGroups from "@/components/UserGroups";
 export default function Home() {
     return (
-        <div className="flex bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#1b143a] from-0% to-[#07011F] to-45% min-h-screen  items-center  justify-items-center overflow-x-hidden">
+        <div className="flex bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#1b143a] from-0% to-[#07011F] to-45% h-screen w-screen  items-center  justify-items-center overflow-x-hidden">
             <UserInfo />
             <div></div>
         </div>
